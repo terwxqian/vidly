c52bffc719631b73297936f1aa7ce892e48dab64
wangxq <wangxq@info2soft.com>
2022-01-16 14:22:11 +0800

    [IM]:sharing templates & url parameters

--- movies/urls.py
+++ movies/urls.py
@@ -9,5 +9,6 @@ from . import views
 # movies/
 # moves/1/detail
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='movies_index'),
+    path('<int:movie_id>', views.detail, name='movies_detail')
 ]
--- movies/views.py
+++ movies/views.py
@@ -11,3 +11,8 @@ def index(request):
     movies = Movie.objects.all()
 
     return render(request, 'movies/index.html', {'movies': movies})
+
+
+def detail(request, movie_id):
+    movie = Movie.objects.get(pk=movie_id)
+    return render(request, 'movies/detail.html', {'movie': movie})
