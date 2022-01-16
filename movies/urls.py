3de0a3930f716965a9cfeba42f37c375e54589da
wangxq <wangxq@info2soft.com>
2022-01-16 16:34:03 +0800

    [IM]:use app_name to adapt the change of url

--- movies/urls.py
+++ movies/urls.py
@@ -1,14 +1,15 @@
 import imp
 from django.urls import URLPattern
 
-
 from django.urls import path
 from . import views
 
-
 # movies/
 # moves/1/detail
+
+app_name = 'movies'
+
 urlpatterns = [
-    path('', views.index, name='movies_index'),
-    path('<int:movie_id>', views.detail, name='movies_detail')
+    path('', views.index, name='index'),
+    path('<int:movie_id>', views.detail, name='detail')
 ]
