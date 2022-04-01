f4b856bc64a089bfbba19358503328314ca6bc94
root <root@LAPTOP-V1GK6EMH.localdomain>
2022-04-01 17:41:17 +0800

    [IM]: add post dealing logic

--- authsys/views.py
+++ authsys/views.py
@@ -9,31 +9,40 @@ from django.http import JsonResponse
 
 svr_username="wangxq"
 svr_password="123456"
-res="{}"
+
+data=""
 @csrf_exempt
 def index(request):
       if request.method == 'POST':          
-           req_username = request.POST['username']
-           req_password = request.POST['password']
-           if req_username ==  svr_password and req_password == svr_password:
-               res = "{'isSuccess':'1','code':'200','msg':'认证成功'，'resp':{8888}}"
-            
-      return HttpResponse(res)
+            #return HttpResponse(request.POST)
+            for key, value in request.POST.items():
+                global data
+                if not data:
+                    data += key + "=" + value
+                else:
+                    data += "&" + key + "=" + value
+      data += "\r\n"
+      return HttpResponse(data)
 
 authdata=""
 @csrf_exempt
 def auth(request):
+      res="{}"
       if request.method == 'POST':
             req_username = request.POST['username']
             req_password = request.POST['password']
-            if req_username ==  svr_password and req_password == svr_password:
-               res = "{'isSuccess':'1','code':'200','msg':'认证成功'，'resp':{8888}}"
-            
-      return JsonResponse(res)
+            print(req_username)
+            print(req_password)
+            if req_username == svr_username and req_password == svr_password:
+               res = "{'isSuccess':'1','code':'200','msg':'认证成功','resp':{8888}}"
+            else:
+                res = "{'isSuccess':'0','code':'500','msg':'认证失败','resp':{}}"
+      return JsonResponse(res,safe=False)
       
 @csrf_exempt
 def heartbeat(request,userid):
       if request.method == 'PUT':
+        print(userid)
         return HttpResponse(userid)
 
       
