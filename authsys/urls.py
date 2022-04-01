6213a1c7ab952e5ba0ccd4df53734ab025efe35a
JefferyStark <57661260+JefferyStark@users.noreply.github.com>
2022-04-01 18:29:55 +0800

    add auth and heartbeat

--- authsys/urls.py
+++ authsys/urls.py
@@ -4,12 +4,14 @@ from django.urls import URLPattern
 from django.urls import path
 from . import views
 
-# /api/auth
-# /api/heartbeat/[userid]
+# api/auth
+# api/heartbeat/[userid]
 
 app_name = 'authsys'
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('api/auth', views.auth, name='auth'),
+    path('api/heartbeat/<userid>', views.heartbeat, name='heatbeat'),
     #url(r'^/api/auth/$', views.MyView.as_view(), name='my-view'),
 ]
--- authsys/views.py
+++ authsys/views.py
@@ -7,10 +7,9 @@ from django.views.generic import View
 from django.views.decorators.csrf import csrf_exempt
 
 data=""
-
 @csrf_exempt
 def index(request):
-      if request.method == 'POST':
+      if request.method == 'POST':          
             #return HttpResponse(request.POST)
             for key, value in request.POST.items():
                 global data
@@ -21,5 +20,22 @@ def index(request):
       data += "\r\n"
       return HttpResponse(data)
 
+authdata=""
+@csrf_exempt
+def auth(request):
+      if request.method == 'POST':
+            for key, value in request.POST.items():
+                global authdata
+                if not authdata:
+                    authdata += key + "=" + value
+                else:
+                    authdata += "&" + key + "=" + value
+      authdata += "\r\n"
+      return HttpResponse(authdata)
+      
+@csrf_exempt
+def heartbeat(request,userid):
+      if request.method == 'PUT':
+        return HttpResponse(userid)
 
       
